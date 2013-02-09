d087c9246057046224298006ded35b305fa433fc
Jelmer <jelmer@ikhoefgeen.nl>
2013-02-09 15:58:32 +0100

    Add talking and whispering animation steps.

--- configuration.php
+++ configuration.php
@@ -22,10 +22,63 @@ function parse_spatial_position($spatial_position, $section)
 	if (!preg_match('/^addressee?\s+\((left|right)\)$/i', $spatial_position, $match))
 		return null;
 
-	if ($section == 'direct and indirect')
-		return 'wispering-' . $match[1];
+	if ($section == 'direct_and_indirect_speech_items')
+		return 'whispering-' . $match[1];
 	else
 		return 'talking-' . $match[1];
+}
+
+function beautify_json($json)
+{
+	$result      = '';
+	$pos         = 0;
+	$strLen      = strlen($json);
+	$indentStr   = '  ';
+	$newLine     = "\n";
+	$prevChar    = '';
+	$outOfQuotes = true;
+
+	for ($i = 0; $i <= $strLen; ++$i)
+	{
+
+		// Grab the next character in the string.
+		$char = substr($json, $i, 1);
+
+		// Are we inside a quoted string?
+		if ($char == '"' && $prevChar != '\\')
+			$outOfQuotes = !$outOfQuotes;
+		
+		// If this character is the end of an element, 
+		// output a new line and indent the next line.
+		else if(($char == '}' || $char == ']') && $outOfQuotes)
+		{
+			$result .= $newLine;
+			$pos --;
+			for ($j=0; $j<$pos; $j++) {
+				$result .= $indentStr;
+			}
+		}
+		
+		// Add the character to the result string.
+		$result .= $char;
+
+		// If the last character was the beginning of an element, 
+		// output a new line and indent the next line.
+		if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes)
+		{
+			$result .= $newLine;
+
+			if ($char == '{' || $char == '[')
+				$pos ++;
+			
+			for ($j = 0; $j < $pos; ++$j)
+				$result .= $indentStr;
+		}
+		
+		$prevChar = $char;
+	}
+
+	return $result;
 }
 
 $f = fopen('./configuration.csv', 'r');
@@ -75,4 +128,4 @@ foreach ($configurations as $section => &$section_configurations)
 	}
 
 header('Content-type: application/json');
-printf('window.configuration = %s;', json_encode($configurations));
+printf('window.configuration = %s;', beautify_json(json_encode($configurations)));
