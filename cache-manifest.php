8db15061cba115b33c1279be00170520bc168a01
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2013-03-17 00:26:27 +0100

    Use one large sprite for all audio (25mb -> less than 4mb? Awesome!)

--- cache-manifest.php
+++ cache-manifest.php
@@ -1,12 +1,7 @@
 <?php
 
 $files = array_merge(
-	glob('assets/audio/conversations/*.wav'),
-	glob('assets/audio/cues/*.wav'),
-	glob('assets/audio/instructions/*.wav'),
-	glob('assets/audio/sprites/*.m4a'),
-	glob('assets/audio/sprites/*.json'),
-	glob('assets/audio/*.wav'),
+	glob('assets/audio/sprites.*'),
 	glob('assets/characters/*.svg'),
 	glob('assets/objects/*.png'),
 	glob('assets/objects/*.svg'),
